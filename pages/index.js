49d9b32a2198e2aa4e0cfff561329f498eb91d84
Ala Obeidat <ala-work@hotmail.com>
2021-10-26 14:03:02 +0300

    Add code

---
+++ pages/index.js
@@ -0,0 +1,68 @@
+import dynamic from "next/dynamic";
+import PropTypes from "prop-types";
+const Navigation = dynamic(() => import("../components/Navigation"));
+const Greetings = dynamic(() => import("../containers/Greetings"));
+const Skills = dynamic(() => import("../containers/Skills"));
+const Proficiency = dynamic(() => import("../containers/Proficiency"));
+const Education = dynamic(() => import("../containers/Education"));
+const Experience = dynamic(() => import("../containers/Experience"));
+const Projects = dynamic(() => import("../containers/Projects"));
+const GithubProfileCard = dynamic(() =>
+	import("../components/GithubProfileCard")
+);
+import { openSource } from "../portfolio";
+import SEO from "../components/SEO";
+
+export default function Home({ githubProfileData }) {
+	return (
+		<div>
+			<SEO
+				data={{
+					title: "Ala Obeidat",
+					description:
+						"A passionate Full Stack Web Developer",
+					image: "https://avatars.githubusercontent.com/u/66348550?v=4",
+					url: "https://alaobeidat.com",
+					keywords: [
+						"Ala",
+						"Obeidat",
+						"Ala Obeidat",
+						"web developer",
+						"full stack",
+						"full stack web developer",
+						"application developer",
+						"dotnet framework",
+						"nodejs ",
+						"dot net",
+						"reactjs ",
+						"api",
+						"redis",
+						"rabbitMQ",
+					],
+				}}
+			/>
+			<Navigation />
+			<Greetings />
+			<Skills />
+			<Proficiency />
+			<Education />
+			<Experience />
+			<Projects />
+			<GithubProfileCard prof={githubProfileData} />
+		</div>
+	);
+}
+
+Home.prototype = {
+	githubProfileData: PropTypes.object.isRequired,
+};
+
+export async function getStaticProps(_) {
+	const githubProfileData = await fetch(
+		`https://api.github.com/users/${openSource.githubUserName}`
+	).then((res) => res.json());
+
+	return {
+		props: { githubProfileData },
+	};
+}
---
+++ portfolio.js
@@ -0,0 +1,204 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+	name: "Ala Obeidat",
+	title: "Hi all, I'm Ala",
+	description:
+		"A passionate Full Stack Web Developer and App Developer having an experience of building Web applications with .Net/ JavaScript / Reactjs / Nodejs  and some other cool libraries and frameworks With C#.",
+	resumeLink: "https://docs.jitbs.co/files/Ala-CV.pdf",
+};
+
+export const openSource = {
+	githubUserName: "ala-obeidat",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+	facebook: "https://www.facebook.com/eng.ala.obeidat", 
+	twitter: "https://twitter.com/ala_N_obeidat",
+	github: "https://github.com/ala-obeidat",
+	linkedin: "https://www.linkedin.com/in/eng-ala-obeidat/",
+};
+
+export const skillsSection = {
+	title: "What I do",
+	subTitle:
+		"CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+	skills: [
+		emoji(
+			"⚡ Develop highly interactive Back end / API for your web and mobile applications"
+		),
+		emoji(
+			"⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"
+		),
+		emoji(
+			"⚡ Integration of third party services such as Firebase/ Redis / RabbitMQ"
+		),
+	],
+
+	softwareSkills: [
+		{
+			skillName: "dot-net",
+			fontAwesomeClassname: "mdi:dot-net",
+		},
+		{
+			skillName: "c-sharp",
+			fontAwesomeClassname: "logos:c-sharp",
+		},
+		{
+			skillName: "sql-database",
+			fontAwesomeClassname: "vscode-icons:file-type-sql",
+		},
+		{
+			skillName: "redis",
+			fontAwesomeClassname: "logos:redis",
+		},
+		{
+			skillName: "rabbitmq",
+			fontAwesomeClassname: "logos:rabbitmq",
+		},
+		{
+			skillName: "firebase",
+			fontAwesomeClassname: "logos:firebase",
+		},
+		{
+			skillName: "nodejs",
+			fontAwesomeClassname: "logos:nodejs-icon",
+		},
+		{
+			skillName: "html-5",
+			fontAwesomeClassname: "vscode-icons:file-type-html",
+		},
+		{
+			skillName: "css3",
+			fontAwesomeClassname: "vscode-icons:file-type-css",
+		},
+		
+		{
+			skillName: "javaScript",
+			fontAwesomeClassname: "vscode-icons:file-type-js",
+		},
+		{
+			skillName: "reactjs",
+			fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+		},
+		{
+			skillName: "python",
+			fontAwesomeClassname: "logos:python",
+		},
+		{
+			skillName: "git",
+			fontAwesomeClassname: "logos:git-icon",
+		}
+	],
+};
+
+export const SkillBars = [
+	{
+		Stack: "Backend",
+		progressPercentage: "90",
+	},
+	{
+		Stack: "Frontend/Design", //Insert stack or technology you have experience in
+		progressPercentage: "40", //Insert relative proficiency in percentage
+	},
+	{
+		Stack: "Applications",
+		progressPercentage: "60",
+	},
+];
+
+export const educationInfo = [
+	{
+		schoolName: "Yarmouk university",
+		subHeader: "Bachelor of Computer engineer",
+		duration: "September 2009 - April 2014",
+		desc: "Involves the design and development of systems based on computers and complex digital logic devices",
+		descBullets: [
+			"These systems find use in such diverse tasks as computation, communication, entertainment, information processing, artificial intelligence, and control.",
+			"Computer engineers will be very much involved with the “Information Highway.”",
+		],
+	}
+];
+
+export const experience = [
+	{
+		role: "Software Developer",
+		company: "T2",
+		companylogo: "/img/icons/common/t2logo.png",
+		date: "Nov 2014 – Apr 2019",
+		desc: "4 years and 6 months",
+		descBullets: [
+			"Work in developing, designing, problem solving and technical discussing with learning new things by work with very expert team",
+			"I have been involved in many structure design sessions for complex and large products",
+		],
+	},
+	{
+		role: "Product Manager",
+		company: "kingscote",
+		companylogo: "/img/icons/common/kingscote-logo.png",
+		date: "May 2020 – Jun 2020",
+		desc: "4 months",
+		descBullets: [
+			"Create a compelling business case to secure the purchase and obtain the funding required to embark on the business engineering journey",
+			"Using RFID technology, designing and programming IOT devices, working on AI technology that fits clients and solves their problems",
+		],
+	},
+	{
+		role: "Software Development Team Lead",
+		company: "Ocost",
+		companylogo: "/img/icons/common/ocost-logo.png",
+		date: "Sep 2015 – Present",
+		desc: "+1 Year",
+		descBullets: [
+			"Analysis, design and development of advance client/server, web based, cloud and n-tier applications",
+			"Lead development team to deliver high quality products on time, analyze system requirements and business needs, perform a research and do the proof of concepts for the new technologies and features",
+		],
+	},
+];
+
+export const projects = [
+	{
+		name: "RiCH",
+		desc: "RiCH is a robust platform that helps enterprise to manage and centralize all their communication in one place",
+		link: "https://t2.sa/en/product/rich",
+	},
+	{
+		name: "Ole5",
+		desc: "Ole5 is a system for tracking and managing huge amounts of internal, incoming and outgoing messages",
+		link: "https://t2.sa/en/product/ole5",
+	},
+	{
+		name: "Supplier Chain",
+		desc: "Supplier Chain is an Ocost product to manage and booking orders and products for client and suppliers",
+		link: "https://dash.ocost.co",
+	},
+	{
+		name: "Lean Lines",
+		desc: "Last mile delevery application to manage orders and delivery process",
+		link: "https://portal.leanoverlines.com",
+	},
+	{
+		name: "Lean Dispatcher",
+		desc: "Lean Dispatcher is a system to manage delivery company fleets",
+		link: "https://portal.leanoverlines.com",
+	},
+	{
+		name: "Rento",
+		desc: "Rento is a system for booking cars and rent it for spacific period",
+		github: "https://github.com/ala-obeidat/Rento",
+	},
+	{
+		name: "StarTrack",
+		desc: "IoT application Tag & Track Solution using RFID technology",
+		github: "https://github.com/ala-obeidat/StarTrack",
+	},
+	{
+		name: "Al-Fajer",
+		desc: "Al-Fajer is a video chat application using WebRTC technology",
+		github: "https://github.com/ala-obeidat/Web-Video-Call",
+	},
+];
+
+export const feedbacks = [];
